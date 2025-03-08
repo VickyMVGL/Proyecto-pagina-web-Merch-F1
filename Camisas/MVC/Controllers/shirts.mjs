7b540f4001479feb027045372acab4cc28f80f18
JoSantana0005 <joseasantana05@gmail.com>
2025-03-07 21:31:50 -0400

    Controladores de camisas listos

--- Camisas/MVC/Controllers/shirts.mjs
+++ Camisas/MVC/Controllers/shirts.mjs
@@ -90,4 +90,48 @@ export class ShirtController{
             });
         }
     }
+
+    // Obtener una camisa por su precio y id
+    getByIDAndPrice = async ( req , res) => {
+        try{
+            const {id,price} = req.params;
+            if(id && price){
+                const shirtsIDPrice = await this.ShirtModels.getByIDAndPrice({id,price});
+                return res.status(200).json(shirtsIDPrice);
+             }
+            else{
+                return res.status(400).json({
+                    message: "El id o el precio no son validos"
+                });
+             }
+        }
+        catch(err){
+            return res.status(500).json({
+                message: "Error al obtener la camisa por id y precio",
+                error: err.message
+            });
+        }
+    }
+
+    // obtener una camisa por su nombre
+    getByName = async ( req , res) => {
+        try{
+            const {name_shirt} = req.params;
+            if(name_shirt){
+                const shirtsName = await this.ShirtModels.getByName({name_shirt});
+                return res.status(200).json(shirtsName);
+            }
+            else{
+                return res.status(400).json({
+                    message: "El nombre no es valido"
+                });
+            }
+        }
+        catch(err){
+            return res.status(500).json({
+                message: "Error al obtener las camisas por nombre",
+                error: err.message
+            });
+        }
+    }
 }

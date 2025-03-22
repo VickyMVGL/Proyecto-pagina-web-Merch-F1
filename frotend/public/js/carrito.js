85b07b9c3eb139d2afcf318c385a4e45b899ca05
VickyMVGL <gonzalezma22@ujap.edu.ve>
2025-03-21 21:56:00 -0400

    Se agrego la funcion de eliminar del carrito y se actualiza el localstorage

--- frotend/public/js/carrito.js
+++ frotend/public/js/carrito.js
@@ -2,6 +2,7 @@
 const carrito = JSON.parse(localStorage.getItem('producto'));
 console.log(carrito);
 // Variables del DOM
+
 const cuenta_productos = document.getElementById('carrito-contador');
 const info_producto = document.getElementById('info-producto-carrito');
 const total = document.getElementById('Total');
@@ -18,6 +19,10 @@ function calcularTotalCarrito(carrito){
 function aumentarCuenta(){
   cuenta_productos.innerHTML = carrito.length;
 }
+// Funcion para disminuir la cuenta
+function disminuirCuenta(){
+  cuenta_productos.innerHTML = carrito.length - 1;
+}
 // Mostrar la informacion de los productos en el carrito
 function mostrarCarrito(carrito){
   let result = calcularTotalCarrito(carrito);
@@ -26,10 +31,10 @@ function mostrarCarrito(carrito){
   // Limpiamos el contenido del carrito
   info_producto.innerHTML = '';
   if(carrito){
-    carrito.forEach(item => {
+    carrito.forEach((item,index) => {
       info_producto.innerHTML += `
         <div class="Content--info">
-                <div class="info-producto-carrito" id="info-producto-carrito">
+                <div class="info-producto-carrito" id="info-producto-carrito" >
                     <div class="producto-carrito">
                         <div class="img-producto">
                             <img src=${item.image} alt="">
@@ -40,16 +45,31 @@ function mostrarCarrito(carrito){
                         </div>
                     </div>
                     <div class="eliminar-producto">
-                        <button class="btn-comprar">Eliminar</button>
+                        <button class="btn-comprar btn-eliminar" data-index=${index}>Eliminar</button>
                     </div>
                 </div>
           </div>
       `
     })
+    const buttonEliminar = document.querySelectorAll(".btn-eliminar");
+      buttonEliminar.forEach(button => {
+        button.addEventListener('click', () => {
+          const index = button.getAttribute('data-index');
+          if(carrito){
+            carrito.splice(index, 1);
+            localStorage.setItem('producto', JSON.stringify(carrito));
+            
+            mostrarCarrito(carrito);
+            actualizarLocalStorage();
+            mostrarCarrito(carrito);
+            actualizarLocalStorage();
+
+            disminuirCuenta();
+          }
+        })
+      })
   }
 }
 
 mostrarCarrito(carrito);
 aumentarCuenta();
-
-
